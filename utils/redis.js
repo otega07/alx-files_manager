378518d48e55df0577329b314926ebdbcc2d4d3d
otega07 <okiriotega@gmail.com>
2024-09-30 13:15:47 +0100

    utils/redis

---
+++ utils/redis.js
@@ -0,0 +1,69 @@
+import { promisify } from 'util';
+import { createClient } from 'redis';
+
+/**
+ * Represents a Redis client.
+ */
+class RedisClient {
+  /**
+   * Creates a new RedisClient instance.
+   */
+  constructor() {
+    this.client = createClient();
+    this.isClientConnected = true;
+    
+    // Handle connection errors
+    this.client.on('error', (err) => {
+      console.error('Redis client failed to connect:', err.message || err.toString());
+      this.isClientConnected = false;
+    });
+    
+    // Handle successful connection
+    this.client.on('connect', () => {
+      this.isClientConnected = true;
+    });
+  }
+
+  /**
+   * Checks if this client's connection to the Redis server is active.
+   * @returns {boolean}
+   */
+  isAlive() {
+    return this.isClientConnected;
+  }
+
+  /**
+   * Retrieves the value of a given key.
+   * @param {String} key The key of the item to retrieve.
+   * @returns {Promise<String | null>} The value of the key, or null if the key does not exist.
+   */
+  async get(key) {
+    const getAsync = promisify(this.client.get).bind(this.client);
+    return await getAsync(key);
+  }
+
+  /**
+   * Stores a key and its value along with an expiration time.
+   * @param {String} key The key of the item to store.
+   * @param {String | Number | Boolean} value The item to store.
+   * @param {Number} duration The expiration time of the item in seconds.
+   * @returns {Promise<void>}
+   */
+  async set(key, value, duration) {
+    const setexAsync = promisify(this.client.setex).bind(this.client);
+    await setexAsync(key, duration, value);
+  }
+
+  /**
+   * Removes the value of a given key.
+   * @param {String} key The key of the item to remove.
+   * @returns {Promise<void>}
+   */
+  async del(key) {
+    const delAsync = promisify(this.client.del).bind(this.client);
+    await delAsync(key);
+  }
+}
+
+export const redisClient = new RedisClient();
+export default redisClient;
