2d8edffd24a7c66667662bbbae9b3ae7a75f46f6
dahunsi-dami <gbemigadare@gmail.com>
2024-10-01 04:02:12 +0100

    Add error handling to getStatus & getStats in AppController.

--- controllers/AppController.js
+++ controllers/AppController.js
@@ -6,18 +6,26 @@ import dbClient from '../utils/db';
 class AppController {
   // for GET /status
   static async getStatus(req, res) {
-    const redisAlive = redisClient.isAlive();
-    const dbAlive = dbClient.isAlive();
+    try {
+      const redisAlive = redisClient.isAlive();
+      const dbAlive = dbClient.isAlive();
 
-    res.status(200).json({ redis: redisAlive, db: dbAlive });
+      res.status(200).json({ redis: redisAlive, db: dbAlive });
+    } catch (error) {
+      console.error(error);
+    }
   }
 
   // for GET /stats
   static async getStats(req, res) {
-    const usersCount = await dbClient.nbUsers();
-    const filesCount = await dbClient.nbFiles();
+    try {
+      const usersCount = await dbClient.nbUsers();
+      const filesCount = await dbClient.nbFiles();
 
-    res.status(200).json({ users: usersCount, files: filesCount });
+      res.status(200).json({ users: usersCount, files: filesCount });
+    } catch (error) {
+      console.error(error);
+    }
   }
 }
 
